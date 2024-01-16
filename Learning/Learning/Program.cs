8bc7657ff1b35503bcd071e360738adb8d61bce6
untitledyet <53141248+untitledyet@users.noreply.github.com>
2024-01-16 14:51:43 +0400

    Task2

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int age = 2004;
+            string name = "Jazz";
+            bool hasCatalyst = true;
+            float engine = 1.3f;
+            double oilUsing = 7.342d;
+            
+            Console.WriteLine("age : " + age);
+            Console.WriteLine("name : " + name);
+            Console.WriteLine("hasCatalyst : " + hasCatalyst);
+            Console.WriteLine("engine : " + engine);
+            Console.WriteLine("oilUsing : " + oilUsing);
+            
+        }
+    }
+}
--- Learning/Learning/obj/Debug/net8.0/Learning.AssemblyInfo.cs
+++ Learning/Learning/obj/Debug/net8.0/Learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c075ce2f39873971bb5340b274ad4997cfbabc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
